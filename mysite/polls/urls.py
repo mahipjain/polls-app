9fd7ceb4a7ee995c5ebda7977b4328ac2c051a6d
Mahip Jain <Mahip Jain>
2016-06-21 13:21:40 +0530

    Register facility added

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
 	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
 	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
 	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+	url(r'^register/$', views.register, name='register'),
 )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
+from django.template import RequestContext
 
 from polls.models import Question, Choice
+from polls.forms import UserForm, UserProfileForm
 
 class IndexView(generic.ListView):
 	template_name = 'polls/index.html'
@@ -38,3 +40,28 @@ def vote(request, question_id):
 		selected_choice.votes +=1
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+def register(request):
+	context = RequestContext(request)
+	registered = False
+	if request.method =='POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+			profile = profile_form.save(commit=False)
+			profile.user = user
+			profile.save()
+			registered = True
+		else:
+			print user_form.errors, profile_form.errors
+	else:
+		user_form =UserForm()
+		profile_form=UserProfileForm()
+	return render_to_response(
+		'polls/register.html',
+		{'user_form':user_form, 'profile_form': profile_form, 'registered': registered},
+		context)
