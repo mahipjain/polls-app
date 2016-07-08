5b1cdccf3864f6e5b965934a98a786276a01593e
Mahip Jain <Mahip Jain>
2016-07-08 15:36:27 +0530

    Frontend changes to login and register

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput())
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'myfieldclass'}))
 	
 	class Meta:
 		model = User
